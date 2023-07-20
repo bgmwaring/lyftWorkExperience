a187fd9ef75867a15e6d034c7f26a18e3611ce18
Benjamin Waring <bgmwaring@gmail.com>
2023-07-20 18:58:17 +0100

    update 2

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,14 +1,9 @@
-from abc import ABC
-
-from car import Car
-
-
-class SternmanEngine(Car, ABC):
+class SternmanEngine():
     def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
         self.warning_light_is_on = warning_light_is_on
+        self.last_service_date = last_service_date
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
         if self.warning_light_is_on:
             return True
         else:
